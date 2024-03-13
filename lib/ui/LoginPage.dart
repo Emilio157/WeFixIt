f88c11b2bdfc8196f94708cb76d174e7206dba3e
Danielpereza <hosky123rd@gmail.com>
2024-03-12 23:42:41 -0600

    Agregando la parte de Samuel

---
+++ lib/ui/LoginPage.dart
@@ -0,0 +1,169 @@
+import 'package:flutter/material.dart';
+import 'package:we_fix_it/ui/widgets/widgetlogin.dart';
+
+class LoginPage extends StatelessWidget {
+  LoginPage({super.key});
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  // methodo de ingreso WIP
+  void signUserIn() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffF4F0E8),
+      appBar: AppBar(
+        toolbarHeight: 100,
+        title: const Text("WeFixIT",
+        style: TextStyle(
+            fontSize: 25,
+        ),),
+        backgroundColor: Colors.white,
+        actions: const [
+          TextButton(onPressed: null, child: Text(
+            "Inicio Sesión usuario",
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),),)
+        ],
+        bottom: const PreferredSize(
+          preferredSize: Size.fromHeight(1),
+          child: Divider(
+              color: Colors.red,
+              thickness: 15,),
+        ),
+      ),
+      body: Center(
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height * 0.8,
+          width: MediaQuery.of(context).size.width * 0.9,
+          child:  Container(
+            decoration: BoxDecoration(
+            color: Colors.grey.shade200,
+            borderRadius: BorderRadius.circular(40),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                    offset: Offset(0, 3),)
+                ]
+            ),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(
+                    Icons.gavel_rounded,
+                    size: 100,
+                  ),
+
+                  const SizedBox(height: 50),
+
+                  const Text(
+                    'Inicia Sesión con tu correo y contraseña para acceder',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                    ),
+                  ),
+
+
+                  const SizedBox(height: 25),
+
+                  const Align(
+                    alignment: Alignment.centerLeft,
+                    child:Text(
+                      '       Correo electrónico',
+                      style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,)
+                    ),
+                  ),
+                  MyTextField(
+                    controller: usernameController,
+                    hintText: 'Ingresa tu correo electrónico',
+                    obscureText: false,
+                  ),
+
+                  const SizedBox(height: 10),
+
+                  const Align(
+                    alignment: Alignment.centerLeft,
+                    child:Text(
+                        '       Contraseña',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,)
+                    ),
+                  ),
+
+                  const SizedBox(height: 10),
+                  MyTextField(
+                    controller: passwordController,
+                    hintText: 'Ingresa tu contraseña',
+                    obscureText: true,
+                  ),
+
+                  const SizedBox(height: 10),
+
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 25.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Olvide mi contraseña',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  const SizedBox(height: 25),
+
+                  MyButtonSignIn(
+                    onTap: signUserIn,
+                  ),
+
+                  const SizedBox(height: 25),
+                  const Padding(
+                    padding: EdgeInsets.only(left: 20, right: 20),
+                    child: Divider(
+                      color: Colors.black,
+                      thickness: 1,),
+                  ),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        '¿No tienes cuenta?',
+                        style: TextStyle(color: Colors.grey[700]),
+                      ),
+                      const SizedBox(width: 4),
+                      const Text(
+                        'Crear cuenta',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/ui/widgets/widgetlogin.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class MyButtonSignIn extends StatelessWidget {
+  final Function()? onTap;
+
+  const MyButtonSignIn({super.key, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(3),
+        margin: const EdgeInsets.symmetric(horizontal: 130),
+        decoration: BoxDecoration(
+          color: Colors.blue,
+          borderRadius: BorderRadius.circular(25),
+        ),
+        child: const Center(
+          child: Text(
+            "Ingresar",
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyTextField extends StatelessWidget {
+  final controller;
+  final String hintText;
+  final bool obscureText;
+
+  const MyTextField({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    required this.obscureText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: TextField(
+        controller: controller,
+        obscureText: obscureText,
+        decoration: InputDecoration(
+            enabledBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.white),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey.shade400),
+            ),
+            fillColor: Colors.white,
+            filled: true,
+            hintText: hintText,
+            hintStyle: TextStyle(color: Colors.grey[500])),
+      ),
+    );
+  }
+}
