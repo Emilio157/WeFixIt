7171b211b14eafa4faffb9b0eb6fa1ddd3b780fb
Samurai327 <sgag2709@gmail.com>
2024-03-13 20:14:01 -0600

    Agregado boton PH para salir sesion

--- lib/ui/homepage.dart
+++ lib/ui/homepage.dart
@@ -1,3 +1,6 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:we_fix_it/ui/report_page.dart'; 
@@ -19,7 +22,7 @@ class _HomePageState extends State<HomePage> {
     Center(child: Text("DIY", style: TextStyle(fontSize: 60),)),
     MyInicio(),
     Center(child: Text("Herramientas", style: TextStyle(fontSize: 60),)),
-    Center(child: Text("Salir", style: TextStyle(fontSize: 60),)),
+    MyLogOut(),
   ];
   @override
   Widget build(BuildContext context) {
@@ -66,7 +69,7 @@ class _HomePageState extends State<HomePage> {
           BottomNavigationBarItem(
               icon: Icon(Icons.exit_to_app),
               //backgroundColor: Colors.red,
-              label: "Salir"),
+              label: "Salir",),
         ],
       ),
     );
@@ -101,7 +104,7 @@ class _MyInicioState extends State<MyInicio> {
                 context,
                 MaterialPageRoute(builder: (context) => MyReportPage(title: "Reportar Problema")),
                 ),
-                child: Text('¡Reportanos un problema!'),
+                child: Text('¡Reportanos un problema!', style: TextStyle(fontSize: 20),),
               ),
         ),
       ),
@@ -118,5 +121,50 @@ class _MyInicioState extends State<MyInicio> {
           )
           ]
     )
+    );
+}
+class MyLogOut extends StatefulWidget {
+  const MyLogOut({super.key});
+
+  @override
+  State<MyLogOut> createState() => _MyLogOutState();
+}
+
+class _MyLogOutState extends State<MyLogOut> {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+      body: Center(
+        child: Container( 
+          height: 200,
+          width: 350,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Color.fromARGB(255, 232, 232, 232),
+          ),
+          child: Column(
+            children: [
+              const SizedBox(height: 25),
+
+              Text("¿Esta seguro que desea cerrar sesión?", style: TextStyle(fontSize: 18),),
+
+              const SizedBox(height: 25),
+
+              ElevatedButton(
+              onPressed: (){},
+              style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromARGB(255, 254, 182, 15),
+                    foregroundColor: Colors.black,
+                    shadowColor: Colors.black,
+                    elevation: 3,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5)),
+                    minimumSize: Size(250, 70), 
+                  ),
+              child: const Text("Cerrar Sesión", style: TextStyle(fontSize: 24),),
+            ),
+            ]          
+          ),
+        ),
+      ),
     );
 }
--- lib/ui/widgets/widgethome.dart
+++ lib/ui/widgets/widgethome.dart
@@ -1,33 +1,33 @@
 import 'package:flutter/material.dart';
 
-class MyButton extends StatelessWidget {
-  final String text;
-  final Function()? picar;
+// class MyButton extends StatelessWidget {
+//   final String text;
+//   final Function()? picar;
 
-  const MyButton({super.key, required this.picar, required this.text});
+//   const MyButton({super.key, required this.picar, required this.text});
 
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: picar,
-      child: Container(
-        padding: const EdgeInsets.all(3),
-        margin: const EdgeInsets.symmetric(horizontal: 130),
-        decoration: BoxDecoration(
-          color: Colors.grey,
-          borderRadius: BorderRadius.circular(5),
-        ),
-        child: Center(
-          child: Text(
-            text,
-            style: const TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontSize: 12,
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return ElevatedButton(
+//       onPressed: picar,
+//       child: Container(
+//         padding: const EdgeInsets.all(3),
+//         margin: const EdgeInsets.symmetric(horizontal: 130),
+//         decoration: BoxDecoration(
+//           color: Colors.grey,
+//           borderRadius: BorderRadius.circular(5),
+//         ),
+//         child: Center(
+//           child: Text(
+//             text,
+//             style: const TextStyle(
+//               color: Colors.white,
+//               fontWeight: FontWeight.bold,
+//               fontSize: 12,
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
