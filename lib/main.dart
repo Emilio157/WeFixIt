020cc2c8302ac946ddb33cb18020b3cce8e30109
Emilio <yeyemilio29@gmail.com>
2024-05-28 17:38:47 -0600

    Mantener sesión iniciada

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:we_fix_it/ui/auth_page.dart';
 import 'firebase_options.dart';
-import 'package:firebase_auth/firebase_auth.dart'; 
-
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  await FirebaseAuth.instance.signOut();
   runApp(MyApp());
 }
 
